ed8b01fc0aca78e90ea54c50a6dc91b533d5f1be
Petr Tripolsky <tripolskypetr@gmail.com>
2022-06-13 03:34:04 +0300

    connection-loader

--- packages/client/src/lib/view/ConnectPageService.ts
+++ packages/client/src/lib/view/ConnectPageService.ts
@@ -3,6 +3,7 @@ import { inject, singleshot } from "react-declarative";
 
 import RouterService from "../base/RouterService";
 import EthersService from "../base/EthersService";
+import LayoutService from "../base/LayoutService";
 
 import TYPES from "../types";
 
@@ -10,18 +11,24 @@ export class ConnectPageService {
 
     readonly ethersService = inject<EthersService>(TYPES.ethersService);
     readonly routerService = inject<RouterService>(TYPES.routerService);
+    readonly layoutService = inject<LayoutService>(TYPES.layoutService);
 
     constructor() {
         makeAutoObservable(this);
     };
 
     handleConnectClick = singleshot(async () => {
-        if (this.ethersService.isMetamaskAvailable) {
-            await this.ethersService.enable()
-                .then(() => this.routerService.push('/mint-page'))
-                .catch(() => this.routerService.push('/permission-page'))
-        } else {
-            this.routerService.push('/nometamask-page');
+        this.layoutService.setLoader(true);
+        try {
+            if (this.ethersService.isMetamaskAvailable) {
+                await this.ethersService.enable()
+                    .then(() => this.routerService.push('/mint-page'))
+                    .catch(() => this.routerService.push('/permission-page'))
+            } else {
+                this.routerService.push('/nometamask-page');
+            }
+        } finally {
+            this.layoutService.setLoader(false);
         }
     });
 
